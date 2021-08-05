f88f319108b71aabf2a1a0f51d71995b92656145
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-08-04 22:35:52 -0300

    trabalhando com singleton

---
+++ LocalizaFrotas/Controllers/SingletonController.cs
@@ -0,0 +1,28 @@
+﻿using LocalizaFrotasInfra.Singleton;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalizaFrotas.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SingletonController : ControllerBase
+    {
+        private readonly SingletonContainer singletonContainer;
+        public SingletonController(SingletonContainer singletonContainer)
+        {
+            this.singletonContainer = singletonContainer;
+        }
+
+        [HttpGet()]
+        public ActionResult Get()
+        {
+            //var singleton = singletonContainer;
+            return Ok(singletonContainer);
+        }
+    }
+}
--- LocalizaFrotas/Startup.cs
+++ LocalizaFrotas/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using LocalizaFrotasInfra.Singleton;
 
 namespace LocalizaFrotas
 {
@@ -26,6 +29,19 @@ namespace LocalizaFrotas
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Localiza.Frotas",
+                    Description = "API - Frotas",
+                    Version = "v1"
+                });
+
+                var apiPath = Path.Combine(AppContext.BaseDirectory, "LocalizaFrotas.xml");
+                c.IncludeXmlComments(apiPath);
+            });
+            services.AddSingleton<SingletonContainer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +51,12 @@ namespace LocalizaFrotas
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API LocalizaFrotas");
+            });
 
             app.UseHttpsRedirection();
 
