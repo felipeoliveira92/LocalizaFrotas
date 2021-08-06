e8d8f84983478d3d182fbe7048efa35615326f28
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-08-05 21:09:07 -0300

    Implementando com Repository

---
+++ LocalizaFrotasDomain/IVeiculoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalizaFrotasDomain
+{
+    public interface IVeiculoRepository
+    {
+        void Add(Veiculo veiculo);
+        void Delete(Veiculo veiculo);
+        void Update(Veiculo veiculo);
+        Veiculo GetById(Guid Id);
+        IEnumerable<Veiculo> GetAll();
+    }
+}
